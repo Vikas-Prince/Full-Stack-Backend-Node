d1ce20ca9c5102c0ec166266a9a6185ff6fd6b83
Vikas-Prince <vikasprince30809@gmail.com>
2025-02-11 21:21:57 +0530

    developed api's for listing page

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
-import { dbConnect, getData } from "./src/controller/dbController";
+import {
+  dbConnect,
+  getData,
+  getDataSort,
+  getDataSortLimit,
+} from "./src/controller/dbController";
+
 dotenv.config();
 let app = express();
 let port = process.env.PORT || 8080;
@@ -42,6 +48,48 @@ app.get("/mealType", async function (req, res) {
   let output = await getData(collection, query);
   res.status(200).send(output);
 });
+
+//filters
+app.get("/filters/:mealId", async (req, res) => {
+  let query = {};
+  let collection = "restaurants";
+  let mealId = Number(req.params.mealId);
+  let cuisineId = Number(req.query.cuisineId);
+  let hcost = Number(req.query.hcost);
+  let lcost = Number(req.query.lcost);
+  let sort = { cost: 1 };
+  let skip = 0;
+  let limit = 10000000000000000;
+
+  if (req.query.sort) {
+    sort = { cost: req.query.sort };
+  }
+
+  if (req.query.skip && req.query.limit) {
+    skip = Number(req.query.skip);
+    limit = Number(req.query.limit);
+  }
+
+  if (cuisineId) {
+    query = {
+      "mealTypes.mealtype_id": mealId,
+      "cuisines.cuisine_id": cuisineId,
+    };
+  } else if (hcost && lcost) {
+    query = {
+      "mealTypes.mealtype_id": mealId,
+      $and: [{ cost: { $gt: lcost, $lt: hcost } }],
+    };
+  } else {
+    query = {
+      "mealTypes.mealtype_id": mealId,
+    };
+  }
+
+  let output = await getDataSortLimit(collection, query, sort, skip, limit);
+  res.status(200).send(output);
+});
+
 
 app.listen(port, (err) => {
   dbConnect();
--- src/controller/dbController.js
+++ src/controller/dbController.js
@@ -25,4 +25,32 @@ async function getData(colName, query) {
   return output;
 }
 
-export { dbConnect, getData };
+async function getDataSort(colName, query, sort) {
+  let output;
+  try {
+    output = await db.collection(colName).find(query).sort(sort).toArray();
+  } catch {
+    output = { Error: "Error in getting data" };
+  }
+
+  return output;
+}
+
+async function getDataSortLimit(colName, query, sort, skip, limit) {
+  let output;
+  try {
+    output = await db
+      .collection(colName)
+      .find(query)
+      .sort(sort)
+      .skip(skip)
+      .limit(limit)
+      .toArray();
+  } catch {
+    output = { Error: "Error in getting data" };
+  }
+
+  return output;
+}
+
+export { dbConnect, getData, getDataSort, getDataSortLimit };
